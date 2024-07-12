12a0624662f8aa99d0d083f7165cb90fbff55b12
Satwik Kaushik <satwikkaushik03@gmail.com>
2024-07-12 17:53:25 +0000

    Modified App.py to work with models trained using new Training code

--- App.py
+++ App.py
@@ -7,24 +7,52 @@ import nltk
 from collections import Counter
 import pandas as pd
 
-# Preprocess text function
+# preprocessing text
 def preprocess_text(text):
     text = re.sub(r'\W', ' ', str(text))
     text = text.lower()
-    tokens = word_tokenize(text)
-    tokens = [word for word in tokens if word not in stopwords.words('english')]
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    return ' '.join(tokens)
 
-# Predict sentiment function
+    # removing urls
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    text = url_pattern.sub(r'', text)
+
+    # removing HTML
+    html_pattern = re.compile('<.*?>')
+    text = html_pattern.sub(r'', text)
+
+    # removing punctuations
+    punctuation_pattern = re.compile(r'[^\w\s]')
+    text = punctuation_pattern.sub(r'', text)
+
+    # tokenization
+    text = word_tokenize(text)
+
+    # removing stopwords
+    stop_words_set = set(stopwords.words("english"))
+    text = [word for word in text if word not in stop_words_set]
+
+    # lemmatization
+    lemmatizer = WordNetLemmatizer()
+    text = [lemmatizer.lemmatize(word) for word in text]
+
+    # sticking sentence back and returning
+    return ' '.join(text)
+
+
+# predicting sentiment
 def predict_sentiment(review):
     processed_review = preprocess_text(review)
-    features = tfidf.transform([processed_review]).toarray()
+    features = tfidf_vectorizer.transform([processed_review]).toarray()
     sentiment = model.predict(features)[0]
-    return sentiment
 
-# Function to read reviews from a CSV and predict sentiment
+    if(sentiment == "pos"):
+        return "positive"
+    elif(sentiment == "neg"):
+        return "negative"
+    else:
+        return "neutral"
+
+# bulk prediction
 def predict_sentiments_from_csv(csv_path):
     try:
         df = pd.read_csv(csv_path, on_bad_lines='skip')
@@ -32,18 +60,17 @@ def predict_sentiments_from_csv(csv_path):
         print(f"Failed to read CSV: {e}")
         return []
     
-    # List to store predicted sentiments
+    # storing predicted sentiments
     predicted_sentiments = []
     
-    # Iterate over each review in the DataFrame
     for review in df['Review']:
         sentiment = predict_sentiment(review)
         predicted_sentiments.append(sentiment)
     
     return predicted_sentiments
 
-# Function to determine the overall sentiment of the course
-def determine_overall_sentiment(sentiments):
+# determining overall sentiment of the course
+def overall_sentiment(sentiments):
     sentiment_counts = Counter(sentiments)
     total_reviews = len(sentiments)
     positive_percentage = (sentiment_counts['positive'] / total_reviews) * 100
@@ -57,15 +84,15 @@ def determine_overall_sentiment(sentiments):
 
 # ------------------------------------------ MAIN ---------------------------------------------
 
-# Load necessary NLTK resources
-nltk.download('punkt', quiet=True)
+# downloading NLTK resources
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 
-# Load the trained model and TfidfVectorizer
-model = load('notebooks/sheep_two.joblib')
-tfidf = load('notebooks/tfidf_vectorizer.joblib')
+# loading model and TfidfVectorizer
+model = load('models/logistic_regression_model.joblib')
+tfidf_vectorizer = load('models/tfidf_vectorizer.joblib')
 
+# basic UI
 print("+---------------------------------------+")
 print("|       Emotions Behind the Screen      |")
 print("|    Sentiment in Reviews of Courses    |")
@@ -75,8 +102,10 @@ print("| 1. Analyze single review              |")
 print("| 2. Analyze csv file(bulk review)      |")
 print("+---------------------------------------+")
 
-# Take user input
+# user input
 user_ask = input(">>> ")
+
+# single review
 if(user_ask == "1"):
     user_input = input("Enter a review: ")
     try:
@@ -85,11 +114,16 @@ if(user_ask == "1"):
     except Exception as e:
         print("Some error occured")
 
+# bulk review
 elif(user_ask == "2"):
     csv_path = input("Enter path of csv file: ")
     try:
         predicted_sentiments = predict_sentiments_from_csv(csv_path)
-        overall_sentiment = determine_overall_sentiment(predicted_sentiments)
+        overall_sentiment = overall_sentiment(predicted_sentiments)
         print(f"The overall sentiment of the course is: {overall_sentiment}")
     except Exception as e:
         print("Some error occured, perhaps a wrong path to csv file")
+
+# wrong input
+else:
+    print("Incorrect Input")
